7fbd3eeada718cd00c89b3c4f39fbe0fd9aff4fb
Guillaume Thomas <32854831+GFThomas@users.noreply.github.com>
2023-07-18 15:38:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ cp GalBar.cc ${NEMO}/usr/thomas/.
 echo "Compile GalBar"
 cd ${NEMO}/usr/thomas
 ${compiler} -o ${NEMO}/obj/acc/GalBar.so GalBar.cc -Iinc/ -Iinc/utils/ -I${NEMO}/inc -I${NEMO}/nemo/lib -D_FILE_OFFSET_BITS=64  -mfpmath=sse -mpreferred-stack-boundary=4 --param inline-unit-growth=50 -ggdb3 -Wall -Wextra -Winit-self  -Wshadow -Wno-format-security -O2 -fPIC -funroll-loops -fforce-addr   -Woverloaded-virtual   -Llib/ -lfalcON -L../utils/lib -lWDutils -L${NEMO}/lib -lnemo -ldl -shared -w
-mv acc/GalBar.so ${NEMO}/obj/acc/.
+#mv acc/GalBar.so ${NEMO}/obj/acc/.
 
 # Use mknemo to load the GalBar potential in GyrfalcON
 echo "Do mknemo gyrfalcON"
