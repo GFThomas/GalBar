8fba2b75b32c5d5ab853b18bdc4d8d6e64e69e0f
Guillaume Thomas <32854831+GFThomas@users.noreply.github.com>
2023-07-18 15:38:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,14 +13,17 @@ compiler="g++"
 
 current_path=`pwd`
 
+# Create the repo thomas
+mkdir ${NEMO}/usr/thomas
+
 # Copy the GalBar file in Nemo
-#cp GalBar.cc ${NEMO}/usr/dehnen/falcON/src/public/acc/.
+cp GalBar.cc ${NEMO}/usr/thomas/.
 
 
 # Compile it
 echo "Compile GalBar"
-cd ${NEMO}/usr/dehnen/falcON/
-${compiler} -o acc/GalBar.so src/public/acc/GalBar.cc -Iinc/ -Iinc/utils/ -I${NEMO}/inc -I${NEMO}/nemo/lib -D_FILE_OFFSET_BITS=64  -mfpmath=sse -mpreferred-stack-boundary=4 --param inline-unit-growth=50 -ggdb3 -Wall -Wextra -Winit-self  -Wshadow -Wno-format-security -O2 -fPIC -funroll-loops -fforce-addr   -Woverloaded-virtual   -Llib/ -lfalcON -L../utils/lib -lWDutils -L${NEMO}/lib -lnemo -ldl -shared -w
+cd ${NEMO}/usr/thomas
+${compiler} -o ${NEMO}/obj/acc/GalBar.so GalBar.cc -Iinc/ -Iinc/utils/ -I${NEMO}/inc -I${NEMO}/nemo/lib -D_FILE_OFFSET_BITS=64  -mfpmath=sse -mpreferred-stack-boundary=4 --param inline-unit-growth=50 -ggdb3 -Wall -Wextra -Winit-self  -Wshadow -Wno-format-security -O2 -fPIC -funroll-loops -fforce-addr   -Woverloaded-virtual   -Llib/ -lfalcON -L../utils/lib -lWDutils -L${NEMO}/lib -lnemo -ldl -shared -w
 mv acc/GalBar.so ${NEMO}/obj/acc/.
 
 # Use mknemo to load the GalBar potential in GyrfalcON
